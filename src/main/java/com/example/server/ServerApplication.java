2d2c441168b5fa9266e72bdb0b3f066bcace592b
MasakiKun <kidconan@gmail.com>
2021-10-10 10:17:55 +0900

    http 요청이 들어왔을 때, 요청 내용을 콘솔에 표시하도록 수정

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -1,13 +1,26 @@
 package com.example.server;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerApplication {
 	public static void main(String[] args) throws Exception {
 		ServerSocket serverSocket = new ServerSocket(8080);
+		System.out.println("Server listen in port 8080...");
 		while(true) {
 			Socket socket = serverSocket.accept();
+			InputStream inputStream = socket.getInputStream();
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			String httpData = "NODATA";
+			int lineCnt = 0;
+			while(!"".equals(httpData)) {
+				httpData = bufferedReader.readLine();
+				lineCnt++;
+				System.out.println(lineCnt + ": " + httpData);
+			}
 			socket.close();
 		}
 	}
